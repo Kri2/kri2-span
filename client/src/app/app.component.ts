c7ff298f568b1a5dbbdabf830d96236ca85cbd87
Krzysztof Turlej <christur@interia.pl>
2018-11-01 17:19:03 +0100

    added bootstrap, styled front page, added cors to backend

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  title = 'kri2-span';
+  constructor(private http:HttpClient){}
+
+  ngOnInit():void{
+    this.http.get<UserResponse>('http://kri2-span.herokuapp.com/api/hello-world')
+    .subscribe(
+        data=>{console.log(data.helloMessage)},
+        error=>{console.log("error occured!!!!!")}
+    )
+  }
+}
+
+interface UserResponse{
+  helloMessage:string;
 }
--- server/src/main/java/io/github/kri2/server/TestController.java
+++ server/src/main/java/io/github/kri2/server/TestController.java
@@ -1,10 +1,12 @@
 package io.github.kri2.server;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins={"https://kri2-span.herokuapp.com","http://localhost:4200"})
 @RequestMapping("/api")
 public class TestController
 {
@@ -13,5 +15,5 @@ public class TestController
         System.out.println("kontroler otrzymał request");
         return "Hello World!";
     }
-
+    
 }
