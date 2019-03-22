4e3ad6254d6a90c35d95f1424a4570b046bf9b9c
Krzysztof Turlej <christur@interia.pl>
2019-03-22 15:10:10 +0100

    temp fix for 404 on heroku when reloading page

--- client/src/app/app-routing/app-routing.module.ts
+++ client/src/app/app-routing/app-routing.module.ts
@@ -5,10 +5,11 @@ import { CarListComponent } from '../car-list/car-list.component';
 import { CarCreateFormComponent } from '../car-create-form/car-create-form.component';
 
 const routes:Routes = [
-  //{ path: '', component:CarListComponent },
-  { path: 'cars', component:CarListComponent },
-  { path: 'form', component:CarCreateFormComponent },
-  { path: '', redirectTo: '/cars', pathMatch: 'full' } //<---- this had some problems when reloading on heroku
+  { path: '', component:CarListComponent },
+  { path: 'cars', component: CarListComponent },
+  { path: 'form', component: CarCreateFormComponent },
+  //{ path: '', redirectTo: '/cars', pathMatch: 'full' }, //<---- this had some problems when reloading on heroku, paths were not available directly
+  //{ path: '**', redirectTo: '/cars' } //<--- for 404 page
 ];
 
 @NgModule({
